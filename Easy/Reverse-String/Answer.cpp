4ed9aacca97b95dcd25932ddced05dd72a43209d
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-15 13:20:31 +0330

    question 3 is solved

--- Easy/Reverse-String/Answer.cpp
+++ Easy/Reverse-String/Answer.cpp
@@ -1,9 +1,12 @@
-class Solution {
+class Solution
+{
 public:
-    void reverseString(vector<char>& s) {
-        int a = 0; 
-        int length = s.size()-1;
-        while(a < length) {
+    void reverseString(vector<char> &s)
+    {
+        int a = 0;
+        int length = s.size() - 1;
+        while (a < length)
+        {
             swap(s[length], s[a]);
             a++;
             length--;
