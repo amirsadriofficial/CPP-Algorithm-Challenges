a0ffce079c4bd4838c7469643e85c202475c270e
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-16 21:55:18 +0330

    question 5 is solved

--- Easy/Length-of-Last-Word/Answer.cpp
+++ Easy/Length-of-Last-Word/Answer.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int size = s.size(), count=0, flag=0;
-        for(int i=size-1; i>=0; i--){
+        for(int i=size-1; i>=0; i--) {
             if(s[i] == ' ' && flag) break;
-            if(s[i] != ' '){
+            if(s[i] != ' ') {
                 flag = 1;
                 count++;
             }
